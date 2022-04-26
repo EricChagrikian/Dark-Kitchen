d2906d592b29fdcd538034620f5018bc12268c14
ricardodo10 <ricardocoutinhosilva1999@gmail.com>
2022-04-26 16:43:51 +0200

    Array 6/8

---
+++ script.js
@@ -0,0 +1,59 @@
+let bouffe = [
+{
+    Name : "Menu Medium Big Burger" ,
+    Ingredient : "800gr de boeuf , 300gr de poulet et 250gr de bacon" ,
+    Accompagnement : "2 kilos de frites cheddar, Menu Large = 3 kilos" ,
+    Sauce : ["BBQ, " ,  "Biggy, " , "Mayo, " , "Ketchup, " , "Andalouse, " , "Samourai, " , "Sauce Carbonara"] ,
+    Boissons : "5 litres de Powerade " , "10 litres pour le large" ,
+    Prix : 15000,
+    Size : 2000 
+}
+{
+    Name : "Menu Mini Medium Big Burger X6" ,
+    Ingredient : "800gr de boeuf , 300gr de poulet et 250gr de bacon" ,
+    Accompagnement : "2 kilos de frites cheddar, Menu Large = 3 kilos" ,
+    Sauce : ["BBQ, " ,  "Biggy, " , "Mayo, " , "Ketchup, " , "Andalouse, " , "Samourai, " , "Sauce Carbonara"] ,
+    Boissons : "5 litres de Powerade " , "10 litres pour le large" ,
+    Prix : 16000,
+    Size : 2500 
+}
+{
+    Name : "Medium Maxi Tacos" , 
+    Ingredient : "Choisis 8 parmis: Poulet mariné, Viande hachée, Cordon Bleu, Tenders, Nuggets, Fricadelle, Falafel, Kebab, Merguez, Bacon, Fish Stick",
+    Accompagnement : "2 kilos de frites cheddar, Menu Large = 3 kilos" ,
+    Sauce : ["BBQ, " ,  "Biggy, " , "Mayo, " , "Ketchup, " , "Andalouse, " , "Samourai, " , "Sauce Carbonara"] ,
+    Boissons : "5 litres de Powerade " ,
+    Prix : 17000,
+
+}
+{
+    Name : "Maxi Maxi Tacos" , 
+    Ingredient : "Choisis 10 parmis: Poulet mariné, Viande hachée, Cordon Bleu, Tenders, Nuggets, Fricadelle, Falafel, Kebab, Merguez, Bacon, Fish Stick",
+    Accompagnement : "2 kilos de frites cheddar, Menu Large = 3 kilos" ,
+    Sauce : ["BBQ, " ,  "Biggy, " , "Mayo, " , "Ketchup, " , "Andalouse, " , "Samourai, " , "Sauce Carbonara"] ,
+    Boissons : "10 litres de Powerade" ,
+    Prix : 20000,
+}
+{
+    Name : "Ramen Tacos " , 
+    Ingredient : "Ramen , Tortilla", 
+    Accompagnement: "Ramen", 
+    Sauce : "Soja" ,
+    Boissons : "10 litres de Powerade" , 
+    Prix : 23000,
+    Size : "3 metrès de Tacos"
+}
+{
+    Name : "Big Boy Pizza",
+    Ingredient : "Viande hachée , Fromage", 
+    Accompagnement : " 2 kilos Bread Sticks",
+    Sauce : "Tomate" , 
+    Boissons : "10 litres de Powerade" , 
+    Prix : 17000,
+    Size : "8 mètres de Diamètre"
+}
+{
+    
+}
+
+]
