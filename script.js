ca478cadc12221feff96359979bf6fa815ad8cc7
ricardodo10 <ricardocoutinhosilva1999@gmail.com>
2022-04-26 16:59:10 +0200

    array 6/8 correct

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ let bouffe = [
     Sauce : ["BBQ, " ,  "Biggy, " , "Mayo, " , "Ketchup, " , "Andalouse, " , "Samourai, " , "Sauce Carbonara"] ,
     Boissons : "5 litres de Powerade " , "10 litres pour le large" ,
     Prix : 15000,
-    Size : 2000 
+    Size : 2000 ,
 }
-{
+,
+{ 
     Name : "Menu Mini Medium Big Burger X6" ,
     Ingredient : "800gr de boeuf , 300gr de poulet et 250gr de bacon" ,
     Accompagnement : "2 kilos de frites cheddar, Menu Large = 3 kilos" ,
     Sauce : ["BBQ, " ,  "Biggy, " , "Mayo, " , "Ketchup, " , "Andalouse, " , "Samourai, " , "Sauce Carbonara"] ,
     Boissons : "5 litres de Powerade " , "10 litres pour le large" ,
     Prix : 16000,
-    Size : 2500 
+    Size : 2500 ,
 }
+,
 {
     Name : "Medium Maxi Tacos" , 
     Ingredient : "Choisis 8 parmis: Poulet mariné, Viande hachée, Cordon Bleu, Tenders, Nuggets, Fricadelle, Falafel, Kebab, Merguez, Bacon, Fish Stick",
@@ -26,6 +28,7 @@ let bouffe = [
     Prix : 17000,
 
 }
+,
 {
     Name : "Maxi Maxi Tacos" , 
     Ingredient : "Choisis 10 parmis: Poulet mariné, Viande hachée, Cordon Bleu, Tenders, Nuggets, Fricadelle, Falafel, Kebab, Merguez, Bacon, Fish Stick",
@@ -34,6 +37,7 @@ let bouffe = [
     Boissons : "10 litres de Powerade" ,
     Prix : 20000,
 }
+,
 {
     Name : "Ramen Tacos " , 
     Ingredient : "Ramen , Tortilla", 
@@ -41,8 +45,9 @@ let bouffe = [
     Sauce : "Soja" ,
     Boissons : "10 litres de Powerade" , 
     Prix : 23000,
-    Size : "3 metrès de Tacos"
+    Size : "3 metrès de Tacos",
 }
+,
 {
     Name : "Big Boy Pizza",
     Ingredient : "Viande hachée , Fromage", 
@@ -50,10 +55,7 @@ let bouffe = [
     Sauce : "Tomate" , 
     Boissons : "10 litres de Powerade" , 
     Prix : 17000,
-    Size : "8 mètres de Diamètre"
-}
-{
-    
+    Size : "8 mètres de Diamètre",
 }
 
 ]
